a18c1f2f927ca22b8b8f83ff0fd4db1251dc5cae
Prince Singh <princesingh@Princes-MacBook-Air.local>
2024-09-29 21:25:58 +0530

    authentication added

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -2,4 +2,6 @@ import Config from 'react-native-config';
 
 export const endpoints = {
   getCatalogue: `${Config.API_BASE_URL}/api/catalogues?populate=image`,
+  login:  `${Config.API_BASE_URL}/api/auth/local`,
+  register: `${Config.API_BASE_URL}/api/auth/local/register`
 };
